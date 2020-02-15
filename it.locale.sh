25c6cb0fab993433769ee085e9fdd20bff099e24
Telefonorosso <36540604+Telefonorosso@users.noreply.github.com>
2020-02-15 23:41:12 +0100

    Add files via upload

---
+++ it.locale.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+MENU_TITLE="Benvenuto! Clicca su un'opzione, poi su OK"
+MENU_CONFIGURE="Configura il WiFi"
+MENU_RESTORELAN="Ripristina la rete cablata"
+MENU_REBOOT="Riavvia il device"
+MENU_SHUTDOWN="Spegni il device"
+MENU_QUIT="Esci da questa utility"
+MENU_DONE="fatto"
+
+CONFIRM_SHUTDOWN="Spengo il device?"
+CONFIRM_REBOOT="Riavvio il device?"
+CONFIRM_RECONFIGURE="Premendo OK la configurazione WiFi verra cancellata e il device verra spento. Dovrai connetterlo via cavo e riaccenderlo per proseguire."
+CONFIRM_TITLE="Conferma"
+
+INFO_BYE="Arrivederci!"
+INFO_SCANNING="Scansione in corso..."
+INFO_TRYING="Tentativo di connessione in corso..."
+INFO_PLEASEWAIT="Attendere prego..."
+INFO_REBOOTING="Riavvio in corso..."
+INFO_SUCCESS="Connessione riuscita!"
+INFO_GOODPASS="Password corretta!"
+
+CONFIGURE_CHOOSEAP="Scegli una rete senza fili"
+CONFIGURE_PASSWORD="Password di rete (lasciare vuoto per reti senza password)"
+
+RESTORELAN_ABORT="Sei gia connesso via cavo!"
+RESTORELAN_CONFIRM="Ripristino la configurazione della rete cablata?"
+
+ERROR_TOOSHORT="Errore, password troppo corta."
+ERROR_BADPASS="Errore, password rifiutata."
+ERROR_WPASUPPL="Errore di connessione"
+ERROR_NETWORK="Errore di rete, la procedura non e andata a buon fine."
